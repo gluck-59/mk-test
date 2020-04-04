e39d842b30a1228db82afa42a093e802666f42c3
gluck59 <support@motokofr.com>
2020-04-04 10:52:43 +0500

    минус реклама motohelplist

--- tools/smarty/compile/%%FD^FD6^FD61021D%%index.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2018-11-18 21:39:17
-         compiled from /Users/gluck/Sites/motokofr.com/themes/Earth/index.tpl */ ?>
-<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
--- tools/smarty/compile/%%FE^FE2^FE2D3559%%feederHeader.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2018-11-18 21:39:06
-         compiled from /Users/gluck/Sites/motokofr.com/modules/feeder/feederHeader.tpl */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('modifier', 'escape', '/Users/gluck/Sites/motokofr.com/modules/feeder/feederHeader.tpl', 1, false),)), $this); ?>
-<link rel="alternate" type="application/rss+xml" title="<?php echo ((is_array($_tmp=$this->_tpl_vars['meta_title'])) ? $this->_run_mod_handler('escape', true, $_tmp, 'htmlall', 'UTF-8') : smarty_modifier_escape($_tmp, 'htmlall', 'UTF-8')); ?>
-" href="<?php echo $this->_tpl_vars['feedUrl']; ?>
-" />
